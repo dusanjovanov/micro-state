8172a0e78747e931fa130845d33a617321a4b8b7
Dusan Jovanov <jovanovdusan1@gmail.com>
2021-09-08 14:36:45 +0200

    useValue now uses empty object to update state

--- src/useValue.tsx
+++ src/useValue.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import { ValueWrapper } from './types';
 
 export function useValue<Value = any>(value: ValueWrapper<Value>): Value {
-  const { 1: setState } = useState(0);
+  const { 1: setState } = useState({});
   useEffect(() => {
-    const l = () => setState(Date.now() + Math.random());
+    const l = () => setState({});
     value.emitter.on('update', l);
     return () => {
       value.emitter.off('update', l);
